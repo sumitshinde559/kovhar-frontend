fd53058013d358c325bd9a44579c1b3cc3ee34da
sumitshinde559 <45029931+sumitshinde559@users.noreply.github.com>
2026-09-19 19:41:50 +1000

    Fix import case for ProfilePage component

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ import CheckoutPage from "./pages/CheckoutPage";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 
-import ProfilePage from "./pages/ProfilePage";
+import ProfilePage from "./pages/Profilepage";
 // import NotFound from "./pages/NotFound";
 
 function App() {
